b48075790b03bb52c1945c71cdf1d6028df81b88
Ayshee <152168245+Ayshee782@users.noreply.github.com>
2025-08-06 23:55:24 +0600

    Add files via upload

---
+++ 1374A-Multiply by 2, divide by 6.py
@@ -0,0 +1,19 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    cnt2 = cnt3 = 0
+    original_n = n
+
+    while n % 2 == 0:
+        cnt2 += 1
+        n //= 2
+
+    while n % 3 == 0:
+        cnt3 += 1
+        n //= 3
+
+    if n != 1 or cnt2 > cnt3:
+        print(-1)
+    else:
+        print(2 * cnt3 - cnt2)
